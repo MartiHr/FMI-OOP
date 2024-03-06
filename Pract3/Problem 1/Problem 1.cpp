ecbfa7e89fdad0db5aeb6cc84a0098c833dff1a9
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-03-06 15:46:01 +0200

    Update Problem 1.cpp

--- Pract3/Problem 1/Problem 1.cpp
+++ Pract3/Problem 1/Problem 1.cpp
@@ -1,10 +1,75 @@
 #include <iostream>
+#include <fstream>
 
+// TODO: refactor using these structs
+struct Cell
+{
+	int cellValue;
+};
 
-void func()
+struct Row 
+{
+	// exract constant
+	Cell cells[5];
+};
+
+struct Table
+{
+	// exract constant
+	Row rows[5];
+};
+
+unsigned getCharCountFromFile(std::ifstream& ifs, char ch)
+{
+	if (!ifs.is_open())
+		return 0;
+
+	unsigned int count = 0;
+
+	char current;
+	while (!ifs.eof())
+	{
+		ifs.get(current);
+		if (current == ch)\
+		{
+			count++;
+		}
+	}
+
+	return count;
+}
+
+unsigned getLinesCount(const char* fileName)
+{
+	std::ifstream myFile(fileName);
+
+	if (!myFile.is_open())
+	{
+		return 0;
+	}
+
+	return getCharCountFromFile(myFile, '\n') + 1;
+}
+
+void matrixToArray()
 {
 
 }
+
+unsigned multiplyMatrices(const char* fileName1, const char* fileName2, const char* resultName)
+{
+	std::ifstream ifs1(fileName1);
+	std::ifstream ifs2(fileName2);
+
+	if (!ifs1.is_open() || !ifs2.is_open())
+	{
+		return 0;
+	}
+
+	
+}
+
+
 
 int main()
 {
