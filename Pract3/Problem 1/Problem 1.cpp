24163ab2010c5fe4e88057cb8c6b70ca4765e971
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-03-06 20:01:19 +0200

    Update Problem 1.cpp

--- Pract3/Problem 1/Problem 1.cpp
+++ Pract3/Problem 1/Problem 1.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 #include <fstream>
 
+const int MAX_TABLE_ROW = 50;
+const int MAX_TABLE_COLS = 50;
+
 // TODO: refactor using these structs
 struct Cell
 {
 	int cellValue;
 };
 
-struct Row 
+struct Row
 {
 	// exract constant
-	Cell cells[5];
+	Cell* cells;
 };
 
 struct Table
 {
 	// exract constant
-	Row rows[5];
+	Row* rows;
 };
 
-unsigned getCharCountFromFile(std::ifstream& ifs, char ch)
+unsigned getCharCountFromFile(std::ifstream& ifs, char soughtChar)
 {
 	if (!ifs.is_open())
+	{
 		return 0;
+	}
 
 	unsigned int count = 0;
 
@@ -30,16 +35,28 @@ unsigned getCharCountFromFile(std::ifstream& ifs, char ch)
 	while (!ifs.eof())
 	{
 		ifs.get(current);
-		if (current == ch)\
+		if (current == soughtChar)
 		{
 			count++;
 		}
 	}
 
-	return count;
+	return count + 1;
 }
 
-unsigned getLinesCount(const char* fileName)
+unsigned getRowsCount(const char* fileName)
+{
+	std::ifstream file(fileName);
+
+	if (!file.is_open())
+	{
+		return 0;
+	}
+
+	return getCharCountFromFile(file, '|');
+}
+
+unsigned getColsCount(const char* fileName)
 {
 	std::ifstream myFile(fileName);
 
@@ -48,28 +65,45 @@ unsigned getLinesCount(const char* fileName)
 		return 0;
 	}
 
-	return getCharCountFromFile(myFile, '\n') + 1;
+	return getCharCountFromFile(myFile, ',');
 }
 
-void matrixToArray()
+void deleteMatrix()
 {
-
+	// TODO: implement
 }
 
-unsigned multiplyMatrices(const char* fileName1, const char* fileName2, const char* resultName)
+int** initMatrix(int matrixRows, const char* fileName)
 {
-	std::ifstream ifs1(fileName1);
-	std::ifstream ifs2(fileName2);
+	std::ifstream myFile(fileName);
 
-	if (!ifs1.is_open() || !ifs2.is_open())
+	if (!myFile.is_open())
 	{
 		return 0;
 	}
 
-	
+	int** matrix = new int* [matrixRows];
+
+
 }
 
+int** multiplyMatrices(const char* fileName1, const char* fileName2, const char* resultName)
+{
+	int firstMatrixRows = getRowsCount(fileName1);
+	int secondMatrixRows = getRowsCount(fileName2);
 
+	if (firstMatrixRows == 0 || secondMatrixRows == 0)
+	{
+		return nullptr;
+	}
+
+	int** matrix1 = initMatrix(firstMatrixRows);
+	int** matrix2 = initMatrix(secondMatrixRows);
+
+	int** resultMatrix = new int* [firstMatrixRows];
+
+
+}
 
 int main()
 {
