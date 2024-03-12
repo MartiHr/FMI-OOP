f18e837ae66e635001cd23ce7dc678f38e5e9d32
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-03-12 20:17:17 +0200

    Update BonusProblem.cpp

--- Pract3/BonusProblem/BonusProblem.cpp
+++ Pract3/BonusProblem/BonusProblem.cpp
@@ -6,25 +6,39 @@ const int MAX_POWER = 1000;
 
 enum class Type
 {
-    NORMAL, 
-    FIRE, 
-    WATER, 
-    GRASS, 
-    ELECTRIC, 
-    GHOST, 
-    FLYING,
+	NORMAL,
+	FIRE,
+	WATER,
+	GRASS,
+	ELECTRIC,
+	GHOST,
+	FLYING,
 };
 
 struct Pokemon
 {
-    char name[MAX_NAME_LEN];
-    Type type;
-    unsigned power;
+	char name[MAX_NAME_LEN + 1];
+	Type type;
+	unsigned power;
 };
 
+void initPokemon(Pokemon& pokemon, char name[MAX_NAME_LEN], Type type, unsigned power)
+{
+	if (power < 10 || power > 100)
+	{
+		std::cout << "Invalid power";
+		return;
+	}
 
+	strcpy(pokemon.name, name);
+	pokemon.type = type;
+	pokemon.power = power;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Pokemon p;
+	char name[] = "Pikachu";
+	initPokemon(p, "Pikachu", Type::ELECTRIC, 10);
+
 }
