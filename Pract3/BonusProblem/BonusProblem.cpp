2191e7f0ab39239cd8324951d632a11ee1adcada
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-03-12 20:39:14 +0200

    Reading from the standard input

--- Pract3/BonusProblem/BonusProblem.cpp
+++ Pract3/BonusProblem/BonusProblem.cpp
@@ -1,6 +1,9 @@
+#pragma warning(disable: 4996)
 #include <iostream>
+#include <cstring>
 
 const int MAX_NAME_LEN = 50;
+const int BUFFER_SIZE = MAX_NAME_LEN + 1;
 const int LEAST_POWER = 10;
 const int MAX_POWER = 1000;
 
@@ -35,10 +38,48 @@ void initPokemon(Pokemon& pokemon, char name[MAX_NAME_LEN], Type type, unsigned 
 	pokemon.power = power;
 }
 
+// Print the type of the pokemon
+void printType(Type type)
+{
+	switch (type)
+	{
+	case Type::NORMAL:
+		std::cout << "Normal" << std::endl;
+		break;
+	case Type::FIRE:
+		std::cout << "Fire" << std::endl;
+		break;
+	case Type::WATER:
+		std::cout << "Water" << std::endl;
+		break;
+	case Type::GRASS:
+		std::cout << "Grass" << std::endl;
+		break;
+	case Type::ELECTRIC:
+		std::cout << "Electric" << std::endl;
+		break;
+	case Type::GHOST:
+		std::cout << "Ghost" << std::endl;
+		break;
+	case Type::FLYING:
+		std::cout << "Flying" << std::endl;
+		break;
+	}
+}
+
+void printPokemon(Pokemon& pokemon)
+{
+	std::cout << pokemon.name << std::endl;
+	printType(pokemon.type);
+	std::cout << pokemon.power << std::endl;
+}
+
 int main()
 {
 	Pokemon p;
-	char name[] = "Pikachu";
-	initPokemon(p, "Pikachu", Type::ELECTRIC, 10);
+	char name[MAX_NAME_LEN] = "Pikachu";
+	initPokemon(p, name, Type::ELECTRIC, 10);
+	printPokemon(p);
+
 
 }
