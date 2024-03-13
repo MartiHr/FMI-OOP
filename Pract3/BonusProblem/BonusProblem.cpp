b95f9a5b69b308039d6f23d02daf65f534b26569
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-03-13 10:49:50 +0200

    Add functions (lazy namings  :anguished: )

--- Pract3/BonusProblem/BonusProblem.cpp
+++ Pract3/BonusProblem/BonusProblem.cpp
@@ -32,7 +32,7 @@ void initPokemon(Pokemon& pokemon, char name[MAX_NAME_LEN], Type type, unsigned 
 {
 	if (power < 10 || power > 100)
 	{
-		std::cout << "Invalid input";
+		std::cout << "Invalid input" << std::endl;
 		return;
 	}
 
@@ -81,7 +81,7 @@ void writePokemonToBinary(std::ofstream& file, Pokemon& pokemon)
 {
 	if (!file.is_open())
 	{
-		std::cout << "File not open";
+		std::cout << "File not open" << std::endl;
 		return;
 	}
 
@@ -92,7 +92,7 @@ void readPokemonFromBinary(std::ifstream& file, Pokemon& pokemon)
 {
 	if (!file.is_open())
 	{
-		std::cout << "File did not open";
+		std::cout << "File did not open" << std::endl;
 		return;
 	}
 
@@ -107,10 +107,10 @@ struct PokemonHandler
 PokemonHandler newPokemonHandler(const char* filename)
 {
 	PokemonHandler ph;
+	ph.filename = " ";
 
 	if (!filename)
 	{
-		ph.filename = " ";
 		return ph;
 	}
 
@@ -140,14 +140,20 @@ Pokemon at(const PokemonHandler& ph, int i)
 	int sizeOfCollection = size(ph);
 
 	Pokemon p;
-	std::ifstream file(ph.filename, std::ios::binary);
+	strcpy(p.name, " ");
+	p.power = 0;
+	p.type = Type::NORMAL;
 
-	if (!file.is_open() || i < 0 || i >= sizeOfCollection)
+
+	if (i < 0 || i >= sizeOfCollection)
 	{
-		strcpy(p.name, " ");
-		p.power = 0;
-		p.type = Type::NORMAL;
+		std::cout << "Index outside of bounds" << std::endl;
+	}
 
+	std::ifstream file(ph.filename, std::ios::binary);
+	if (!file.is_open())
+	{
+		std::cout << "File did not open" << std::endl;
 		return p;
 	}
 
@@ -163,6 +169,7 @@ void swap(const PokemonHandler& ph, int i, int j)
 
 	if (i < 0 || i >= sizeOfCollection || j < 0 || j >= sizeOfCollection)
 	{
+		std::cout << "Index outside of bounds" << std::endl;
 		return;
 	}
 
@@ -176,29 +183,41 @@ void swap(const PokemonHandler& ph, int i, int j)
 		return;
 	}
 
+	int currentP = file.tellp();
+
 	file.seekp(j * sizeof(Pokemon), std::ios::beg);
 	writePokemonToBinary(file, first);
+
+	file.seekp(i * sizeof(Pokemon), std::ios::beg);
 	writePokemonToBinary(file, second);
+
+	file.seekp(currentP, std::ios::beg);
 }
 
 int main()
 {
 	// Read a pokemon from the standard input and print it
 	Pokemon p1;
-	char name[] = "Pikachu";
-	initPokemon(p1, name, Type::ELECTRIC, 10);
+	char name1[] = "Pikachu";
+	initPokemon(p1, name1, Type::ELECTRIC, 10);
 	printPokemon(p1);
+
+	Pokemon p2;
+	char name2[] = "Charizard";
+	initPokemon(p2, name2, Type::FIRE, 30);
+	printPokemon(p2);
 
 	// Write a pokemon to a binary file
 	std::ofstream outFile(FILE_NAME, std::ios::binary);
 	writePokemonToBinary(outFile, p1);
+	writePokemonToBinary(outFile, p2);
 	outFile.close();
 
 	// Read a pokemon from a binary file
 	std::ifstream inFile(FILE_NAME, std::ios::binary);
-	Pokemon p2;
-	readPokemonFromBinary(inFile, p2);
-	printPokemon(p2);
+	Pokemon pokemonFromBinary;
+	readPokemonFromBinary(inFile, pokemonFromBinary);
+	printPokemon(pokemonFromBinary);
 	inFile.close();
 
 	//Pokemon handler functions
@@ -210,8 +229,25 @@ int main()
 	std::cout << size(ph) << std::endl;
 
 	// Print pokemon at index
+	Pokemon invalid = at(ph, -1);
 	Pokemon newP = at(ph, 1);
 	printPokemon(newP);
 
+	// Swap pokemons
+	Pokemon first = at(ph, 0);
+	printPokemon(first);
 
+	Pokemon second = at(ph, 1);
+	printPokemon(second);
+
+	swap(ph, 1, 2);
+	swap(ph, 0, 1);
+	
+	first = at(ph, 0);
+	second = at(ph, 1);
+	
+	printPokemon(first);
+	printPokemon(second);
+
+	return 0;
 }
