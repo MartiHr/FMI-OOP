89c6627917dbc40316712a3ed6ec492dcd33f121
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-05-25 20:12:05 +0300

    Update

--- Practice For Exam/W10/Sets/Sets/SetByCriteria.h
+++ Practice For Exam/W10/Sets/Sets/SetByCriteria.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "DynamicSet.h"
+
+class SetByCriteria : private DynamicSet
+{
+private:
+    void fillSet(bool (*criteria)(unsigned n));
+
+public:
+    SetByCriteria(unsigned n, bool (*incl)(unsigned n));
+    void setInclude(bool (*incl)(unsigned n));
+
+    using DynamicSet::print;
+    using DynamicSet::contains;
+};
